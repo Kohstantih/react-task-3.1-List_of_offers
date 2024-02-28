ddd78c19b886580e8da9aa40cfcd3ec225977adc
Konstantin <117026423+Kohstantih@users.noreply.github.com>
2024-02-28 18:24:52 +0300

    add func

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,6 +1,7 @@
 import ItemProps from "../interfaces/ItemProps";
 import definitionQuantityStatus from "./definitionQuantityStatus";
 import validationPrice from "./validationPrice";
+import validationTitle from "./validationTitle";
 
 export default function Item({ obj }: ItemProps) {
     if (obj.state === 'removed') return;
@@ -13,7 +14,7 @@ export default function Item({ obj }: ItemProps) {
                 </a>
             </div>
             <div className="item-details">
-                <p className="item-title">{obj.title}</p>
+                <p className="item-title">{validationTitle(obj.title)}</p>
                 <p className="item-price">{`${validationPrice(obj.currency_code)}${obj.price}`}</p>
                 <p className={`item-quantity ${definitionQuantityStatus(obj.quantity)}`}>{`${obj.quantity} left`}</p>
             </div>
